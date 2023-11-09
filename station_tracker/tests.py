e8bb4d2a141e3667e6d3e53ecd8c82a20df0b404
Himaja-Ryali <123218815+Himaja-Ryali@users.noreply.github.com>
2023-11-09 03:05:18 +0000

    Sprint1- First Implementation and Testing - added test cases

--- station_tracker/tests.py
+++ station_tracker/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.test import Client  # Use Django's test client
+
+class MyTestCase(TestCase):
+
+    def test_user_signup_view(self):
+        client = Client()  # Create a test client
+        response = client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_user_login_view(self):
+        client = Client()  # Create a test client
+        response = client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_logout_view(self):
+         client = Client()  # Create a test client
+         response = client.get(reverse('logout'))
+         self.assertEqual(response.status_code, 302)  # 302 is the status code for a redirect
+         self.assertFalse(response.context['user'].is_authenticated)
+
