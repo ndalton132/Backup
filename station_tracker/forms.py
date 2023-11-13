42dca7a0a4b964bc279103c5c54b17eaa7d4c731
Himaja-Ryali <123218815+Himaja-Ryali@users.noreply.github.com>
2023-11-13 18:21:46 +0000

    Modified UI and added logo

--- station_tracker/forms.py
+++ station_tracker/forms.py
@@ -3,10 +3,19 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    class Meta:
+  email = forms.EmailField(required=True)
+  
+  class Meta:
         model = User 
         fields = ['username', 'password1', 'password2']
-
+      
+  def save(self, commit=True):
+      user = super(SignupForm, self).save(commit=False)
+      user.email = self.cleaned_data['email']
+      if commit:
+        user.save()
+        return user
+        
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
--- station_tracker/urls.py
+++ station_tracker/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-#from .views import home
+from .views import index
 from . import views
 
 urlpatterns = [
    # path('', home,name="home"),
    path('', views.index, name='home'),
+   path('index/', index, name='index'),
    path('login/', views.user_login, name='login'),
    path('signup/', views.user_signup, name='signup'),
    path('logout/', views.user_logout, name='logout'),
